846d87a25b94536dac82e05dc037ecec35bfba3d
eswar <eswar@gmail.com>
2024-02-09 12:43:19 +0530

    updated

--- src/Componentess/LformGroupExample.js
+++ src/Componentess/LformGroupExample.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { FaFacebook, FaGoogle, FaTwitter } from "react-icons/fa";
-import Form from 'react-bootstrap/Form';
 import Image from 'react-bootstrap/Image';
 import Button from 'react-bootstrap/Button';
 import FormGroupExample from './FormGroupExample';
