10b1008e06a83344ed039fca024efe57f03e2947
eswar <eswar@gmail.com>
2024-02-09 10:45:22 +0530

    update

--- src/Componentess/Logo.js
+++ src/Componentess/Logo.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Logo() {
   return (
     <div className='ten'>
-        <img src='https://diy-assets.classplus.co/_next/image?url=https://cdn-diy-public.classplus.co/prod/Scoogle%20Logo%20PNG_1673868755859.png&w=1920&q=75' style={{height:'50px',marginRight:'500px'}}></img>
+        <img src='https://diy-assets.classplus.co/_next/image?url=https://cdn-diy-public.classplus.co/prod/Scoogle%20Logo%20PNG_1673868755859.png&w=1920&q=75' alt='' style={{height:'50px',marginRight:'500px'}}></img>
 
     </div>
   )
--- src/Componentess/Team.js
+++ src/Componentess/Team.js
@@ -4,10 +4,10 @@ export default function Team() {
   return (
     <div>
         <center style={{marginTop:'200px'}}>
-        <img src='https://diy-assets.classplus.co/404Text.svg'></img>
+        <img src='https://diy-assets.classplus.co/404Text.svg' alt=''></img>
         <h1>Page Not Found</h1>
         <h2>We are sorry, the page you requested could not be found</h2>
-        <img src="https://diy-assets.classplus.co/plug.svg"></img>
+        <img src="https://diy-assets.classplus.co/plug.svg" alt=''></img>
         </center>
     </div>
   )
